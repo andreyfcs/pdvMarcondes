ec6a2c0bdc4c03978cb3e64bd1f0777e9f1e5154
Andrey Santos <andrey@andrey.fcs>
2024-11-23 12:00:15 -0300

    escalar api, frontend

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -1,10 +1,25 @@
-"use client"
-import Link from "next/link"
-
-const Dashboard = () => {
+// app/products/page.js
+export default async function Products() {
+    // Faz a requisição para a API
+    const res = await fetch('http://localhost:3001/api/products');
+    
+    // Converte a resposta para JSON
+    const products = await res.json();
+  
     return (
-<h1>olá</h1>
-    );
-};
+      <div className="text-center bg-gray-200 border border-2 border-rose-300">
+        <h1>Lista de Produtos registrados no banco de dados</h1>
+        <input />
+        <button div className="border-rose-300 border-2">Enviar</button>
 
-export default Dashboard;   
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              {product.name} - {product.price}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  
